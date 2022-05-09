28cecc15176851c61afe14293e8cf0a8f5ae0047
jaxydog <32988901+jaxydog@users.noreply.github.com>
2022-05-09 18:43:57 -0400

    Fix ping calculation

--- source/declaration/command/ping.ts
+++ source/declaration/command/ping.ts
@@ -1,9 +1,11 @@
-import { CommandInteraction } from "discord.js"
+import { CommandInteraction, Message } from "discord.js"
 import { Action } from "../../internal/action"
 import { Embed } from "../../wrapper/embed"
 
 export const action = new Action<CommandInteraction>("command/ping").fetchData().invokes(async (interact) => {
-	const delay = Date.now() - interact.createdTimestamp
-	const embed = new Embed().title(`Pong! (${delay}ms)`)
+	const embed = new Embed().title(`Pong! (...)`)
 	await interact.reply({ embeds: [embed.build()], ephemeral: true })
+	const reply = (await interact.fetchReply()) as Message
+	const delay = reply.createdTimestamp - interact.createdTimestamp
+	await interact.editReply({ embeds: [embed.title(`Pong! (${delay} ms)`).build()] })
 })
